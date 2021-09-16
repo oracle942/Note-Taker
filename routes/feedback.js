c15137e689ef278b67824d8759e5d3b66105c889
Gerardo Sanchez <oracle942@gmail.com>
2021-09-15 19:42:52 -0700

    Added routes and helper files

---
+++ routes/feedback.js
@@ -0,0 +1,40 @@
+const notes = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
+
+// GET Route for retrieving the notes
+
+notes.get('/notes', (req, res) => {
+  console.info(`${req.method} request received for notes`);
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for submitting notes
+notes.post('/', (req, res) => {
+  console.info(`${req.method} request received to save notes`);
+
+  // Destructuring assignment for the items in req.body
+  const { notes } = req.body;
+
+  // If all the required properties are present
+  if (notes) {
+    // Variable for the object we will save
+    const newNotes = {
+      note,
+      note_id: uuid(),
+    };
+
+    readAndAppend(newFeedback, './db/feedback.json');
+
+    const response = {
+      status: 'success',
+      body: newFeedback,
+    };
+
+    res.json(response);
+  } else {
+    res.json('Error in posting feedback');
+  }
+});
+
+module.exports = fb;
--- server.js
+++ server.js
@@ -2,15 +2,11 @@ const express = require('express')
 const { readFromFile, writeToFile, readAndAppend } = require('./helpers/fsUtils.js')
 const path = require('path')
 const app = express()
-const fs = requrie('fs')
+const fs = require('fs')
+const util = require('util')
+const uuid = require('./helpers/uuid');
+
 const PORT = process.env.PORT || 5000;
-
-
-
-//Init middleware
-// app.use(logger)
-
-
 
 // Body Parser Middleware
 app.use(express.json());
@@ -20,11 +16,18 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.static('public'))
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+app.post('/notes', (req, res) => req.sendFile(path.join(__dirname, '/db/db.html')));
+
+
 
 //Notes API route
 app.get('/api/notes', (req, res) => {
 
 })
+
+app.post('/db/db'), (req,res) => {
+    console.log(req.body)
+}
 // app.use('./public/notes', require('./public/notes'))
 
 
